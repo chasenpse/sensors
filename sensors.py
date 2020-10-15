90e1efb71237db2e42059df9948d6dfcfd5aea1e
Nick Scardino <nick@scardino.dev>
2020-10-15 16:15:55 -0400

    simplified script

--- sensors.py
+++ sensors.py
@@ -5,34 +5,22 @@ import time
 import os
 from datetime import datetime
 
-def setFilePath():
-    global d, fullFilePath
-    d = datetime.now()
-    initYear = "%04d" % (d.year)
-    initMonth = "%02d" % (d.month)
-    initDay = "%02d" % (d.day)
-    fullFilePath = f"{os.path.dirname(os.path.realpath(__file__))}/log/{initYear}{initMonth}{initDay}.log"
-
-setFilePath()
+d = datetime.now()
+initYear = "%04d" % (d.year)
+initMonth = "%02d" % (d.month)
+initDay = "%02d" % (d.day)
+initHour = "%02d" % (d.hour)
+initMins = "%02d" % (d.minute)
+initSecs = "%02d" % (d.second)
+fullFilePath = f"{os.path.dirname(os.path.realpath(__file__))}/log/{initYear}{initMonth}{initDay}.log"
 
 DHT_SENSOR = Adafruit_DHT.DHT22
 
 # Change this to the GPIO pin used (not the pin number)
 DHT_PIN = 14
 
-while True:
-    humidity, temp = Adafruit_DHT.read_retry(DHT_SENSOR, DHT_PIN)
+humidity, temp = Adafruit_DHT.read_retry(DHT_SENSOR, DHT_PIN)
 
-    # Check for new day, update `fullFilePath` if true:
-    if datetime.now().day != d.day:
-        setFilePath()
-    
-    with open(fullFilePath,mode='a') as f:
-        if humidity is not None and temp is not None:
-            tmp = datetime.now()
-            initHour = "%02d" % (tmp.hour)
-            initMins = "%02d" % (tmp.minute)
-            initSecs = "%02d" % (tmp.second)
-            f.write(f"[{initHour}:{initMins}:{initSecs}] *** Temp={(temp * 9/5) + 32:.1f} Humidity={humidity:.1f}\n")
-
-    time.sleep(5)
+with open(fullFilePath,mode='a') as f:
+    if humidity is not None and temp is not None:
+        f.write(f"[{initHour}:{initMins}:{initSecs}] *** Temp={(temp * 9/5) + 32:.1f} Humidity={humidity:.1f}\n")
