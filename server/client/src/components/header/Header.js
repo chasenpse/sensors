5633b2cfa12af1f7255bbaae31a0e9d98e9a2076
Nick Scardino <nick@scardino.dev>
2020-12-23 08:51:35 -0500

    changed default range to past 48 hours tweaked breakpoints changed title copy to hostname

--- server/client/src/components/header/Header.js
+++ server/client/src/components/header/Header.js
@@ -2,8 +2,9 @@ import React from 'react';
 import './Header.css';
 import Title from "./title/Title";
 import Controls from "./controls/Controls";
+import { hostname } from 'os';
 
-const title = <h1>sen<span>Pi</span></h1>;
+const title = <h1>{hostname()}</h1>;
 
 const Header = () => {
     return (
--- server/client/src/utils/getDate.js
+++ server/client/src/utils/getDate.js
@@ -2,7 +2,7 @@ const today = new Date();
 const date = `${today.getFullYear()}-${String(today.getMonth() + 1).padStart(2, '0')}-${String(today.getDate()).padStart(2, '0')}`;
 
 const startDate = new Date();
-startDate.setDate(startDate.getDate()-5);
+startDate.setDate(startDate.getDate()-1);
 export const defaultStartDate = `${startDate.getFullYear()}-${String(startDate.getMonth() + 1).padStart(2, '0')}-${String(startDate.getDate()).padStart(2, '0')}`;
 
 export default date;
