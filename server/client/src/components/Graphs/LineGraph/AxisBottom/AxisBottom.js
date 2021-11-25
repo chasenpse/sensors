5880b1c3ac55f4621370aa163a33fe38175c4f98
Nick Scardino <nick@scardino.dev>
2021-11-25 00:06:46 -0500

    revised graphs, improved mobile views

--- server/client/src/components/Graphs/LineGraph/AxisBottom/AxisBottom.js
+++ server/client/src/components/Graphs/LineGraph/AxisBottom/AxisBottom.js
@@ -1,11 +1,12 @@
 import './AxisBottom.css';
 
-export const AxisBottom = ({xScale, innerHeight, tickFormat, tickOffset = 3}) =>
-    xScale.ticks().map(tickValue => (
+export const AxisBottom = ({xScale, innerHeight, tickFormat, tickOffset = 3}) => {
+    return xScale.ticks().map(tickValue => (
         <g className="tick bottom" key={tickValue} transform={`translate(${xScale(tickValue)},0)`}>
             <line className="line bottom" y2={innerHeight}/>
-            <text style={{textAnchor: 'middle'}} y={innerHeight + tickOffset}>
+            <text className={"bottomAxisText"} x={-1*(innerHeight + tickOffset)/1.47} y={(innerHeight + tickOffset)/1.45} transform={`rotate(-45)`}>
                 {tickFormat(tickValue)}
             </text>
         </g>
     ));
+}
