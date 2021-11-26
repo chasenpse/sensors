0c4f9d1a1e92e88197f2da5810dcca31af26432f
Nick Scardino <nick@scardino.dev>
2021-11-26 09:25:41 -0500

    more axis line and label tweaks

--- server/client/src/components/Graphs/LineGraph/AxisBottom/AxisBottom.js
+++ server/client/src/components/Graphs/LineGraph/AxisBottom/AxisBottom.js
@@ -4,7 +4,7 @@ export const AxisBottom = ({xScale, innerHeight, tickFormat, tickOffset = 3}) =>
     return xScale.ticks().map(tickValue => (
         <g className="tick bottom" key={tickValue} transform={`translate(${xScale(tickValue)},0)`}>
             <line className="line bottom" y2={innerHeight}/>
-            <text className={"bottomAxisText"} x={-1*(innerHeight + tickOffset)/1.47} y={(innerHeight + tickOffset)/1.45} transform={`rotate(-45)`}>
+            <text className={"bottomAxisText"} x={-1*(innerHeight + tickOffset)/1.52} y={(innerHeight + tickOffset)/1.47} transform={`rotate(-45)`}>
                 {tickFormat(tickValue)}
             </text>
         </g>
--- server/client/src/components/Graphs/LineGraph/AxisLeft/AxisLeft.js
+++ server/client/src/components/Graphs/LineGraph/AxisLeft/AxisLeft.js
@@ -4,7 +4,7 @@ export const AxisLeft = ({yScale, innerWidth, tickOffset = 3}) =>
     yScale.ticks().map(tickValue => (
         <g key={tickValue} className="tick left" transform={`translate(0,${yScale(tickValue)})`}>
             <line className="line left" x2={innerWidth}/>
-            <text className={'leftAxisText'} x={-tickOffset} dy={5}>
+            <text className={'leftAxisText'} x={-tickOffset} y={-1}>
                 {tickValue}
             </text>
         </g>
